d3c6ffdc4b04e191e00b6cb8147c84b4250d7841
johnbarneta <jgomez@ideosoft.es>
2016-05-02 00:24:15 +0200

    fix BaseStalkerController

--- admin/src/Controller/BaseStalkerController.php
+++ admin/src/Controller/BaseStalkerController.php
@@ -105,9 +105,9 @@ class BaseStalkerController {
         $this->app['action_alias'] = (count($tmp) == 2) ? $tmp[1] : '';
         $this->baseHost = $this->request->getSchemeAndHttpHost();
         $this->workHost = $this->baseHost . \Config::getSafe('portal_url', '/stalker_portal/');
-        $this->relativePath = Config::getSafe('portal_url', '/stalker_portal/');
+        $this->relativePath = \Config::getSafe('portal_url', '/stalker_portal/');
         $this->app['workHost'] = $this->workHost;
-        $this->app['relativePath'] = Config::getSafe('portal_url', '/stalker_portal/');
+        $this->app['relativePath'] = \Config::getSafe('portal_url', '/stalker_portal/');
         $this->app['workHost'] = $this->workHost;
         $this->Uri = $this->app['request']->getUri();
         $controller = (!empty($this->app['controller_alias']) ? "/" . $this->app['controller_alias'] : '');
